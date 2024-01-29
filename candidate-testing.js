0a18b01d8fa199478e207b4888334ec46dfa1f95
Doss Stone <Doss.s.stl@gmail.com>
2024-01-28 23:28:23 -0600

    completed candidate testing

--- candidate-testing.js
+++ candidate-testing.js
@@ -14,7 +14,7 @@ let candidateAnswer = "";
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
-let correctCandidateAnswers = 0;
+
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -31,6 +31,7 @@ function askQuestion() {
 
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  let correctCandidateAnswers = 0;
 for (let i = 0; i < questions.length; i++){
   if (candidateAnswers[i].toUpperCase() === correctAnswers[i].toUpperCase()){
   console.log (`Correct! You answered ${candidateAnswers[i]} and the correct answer is ${correctAnswers[i]}`);
@@ -42,7 +43,7 @@ for (let i = 0; i < questions.length; i++){
 }}
 
 //TODO 3.2 use this variable to calculate the candidates score.
-let grade = correctCandidateAnswers/questions.length * 100; 
+let grade = (correctCandidateAnswers/questions.length) * 100; 
 if (grade >= 80){
   console.log(`Congrats! You have passed this test with a score of ${grade}%`);
   } else {
